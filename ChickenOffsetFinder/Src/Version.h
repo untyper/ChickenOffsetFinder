bd84ce822201c09c42bde4d124f201a1dd7dcdc1
untyper <76541296+untyper@users.noreply.github.com>
2025-05-19 17:01:39 +0200

    Version

--- ChickenOffsetFinder/Src/Version.h
+++ ChickenOffsetFinder/Src/Version.h
@@ -5,6 +5,6 @@
 #define COF_LICENSE_YEAR "2024-2025"
 #define COF_AUTHOR       "Jovan J. E. Odassius <github@untyper>"
 #define COF_NAME         "Chicken Offset Finder"
-#define COF_VERSION      "1.0.0"
+#define COF_VERSION      "1.0.1"
 
 #endif // !COF_VERSION_INFORMATION
